588c559d6bda02c02f37dbc13b16cfa8daeaba57
Orlando Soto <Orlando.Soto-Pastor@cfpb.gov>
2014-12-04 10:29:31 -0500

    updated Jmeter version from 2.11 to version 2.12

--- bin/JMeterInstaller.py
+++ bin/JMeterInstaller.py
@@ -4,10 +4,10 @@ import os, sys, hashlib, urllib2, tempfile, zipfile, distutils.core
 class JMeterInstaller(object):
 
     def __init__(self):
-        self.jmeter_version = "2.11"
+        self.jmeter_version = "2.12"
         self.jmeter_dir = "apache-jmeter-%s/" % self.jmeter_version
         self.download_dir = tempfile.mkdtemp() + "/"
-        self.hashes = {"jmeter.zip": "c7efb7d1e950caeb5a5720bf0b2445893ca8fe61",
+        self.hashes = {"jmeter.zip": "737f33e1164f636d442f26fa4028a3aad7d579b5",
                        "jmp-standard.zip": "5df124bc039a3cef291a3e9054110a1ff1ae8441",
                        "jmp-extras.zip": "1f6ac7c3200a5d9f42f55217bf0c287fdbba485c",
                        "jmp-extraslibs.zip": "1087fdf9506ecfed202bea7625a227f6bdc14918"}
